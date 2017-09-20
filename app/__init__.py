38d120a8251d5fca8e0906e9b292fd95dec20222
generalave <icepicktoeye@gmail.com>
2017-09-20 15:01:15 -0400

    nuke and start over

--- app/__init__.py
+++
@@ -1,2 +0,0 @@
-from .App       import App   # App initializer class
-from .Constants import *     # Constants file
--- app/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-from .NES import NES   # Nintendo Entertainment System Controller
-from .N64 import N64   # Nintendo 64 Controller
